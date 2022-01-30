57a13d8fbb71b0b4bb3998baa2773695dedf4acb
AlexWilliam <alex.william@outlook.com>
2022-01-29 21:14:06 -0300

    Nomeando rotas

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,22 @@ use Illuminate\Support\Facades\Route;
     return 'Olá, seja bem-vindo ao laravel!';
 });*/
 
-Route::get("/", "PrincipalController@principal");
+Route::get("/", "PrincipalController@principal")->name("site.index");
+Route::get("/sobre-nos", "SobreNosController@sobreNos")->name("site.sobrenos");
+Route::get("/contato", "ContatoController@contato")->name("site.contato");
+Route::get("/login", function(){ return "Login"; })->name("site.login");
 
-Route::get("/sobre-nos", "SobreNosController@sobreNos");
+// /admin
+Route::prefix("/admin")->group(function(){
+    
+    Route::get("/", function(){ return "admin"; })->name("admin");
+    Route::get("/clientes", function(){ return "clientes"; })->name("admin.clientes");
+    Route::get("/fornecedores", function(){ return "fornecedores"; })->name("admin.fornecedores");
+    Route::get("/produtos", function(){ return "produtos"; })->name("admin.produtos");
 
-Route::get("/contato", "ContatoController@contato");
+});
 
-// nome, categoria, assunto, mensagem
+/*// nome, categoria, assunto, mensagem
 Route::get("/contato/{nome?}/{idCategoria?}", 
     function(string $nome="Desconhecido", int $idCategoria=1){
         echo "Estamos aqui! Bem-vindo {$nome}. Sua categoria {$idCategoria}!";
@@ -38,7 +47,7 @@ Route::get("/contato/{nome}/{idCategoria}",
         echo "Estamos aqui! Bem-vindo {$nome}. Sua categoria {$idCategoria}!";
     }
 )->where('nome', '[A-Za-z]+')->where("idCategoria", "[0-9]+");
-// Route::get($uri, $callback);
+// Route::get($uri, $callback);*/
 
 /* verbo http
 
