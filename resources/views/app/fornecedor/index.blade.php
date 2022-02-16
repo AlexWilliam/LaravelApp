4882fe0c1260e2d2906eb3bc55392f03408909b3
AlexWilliam <alex.william@outlook.com>
2022-02-15 23:11:20 -0300

    inicializando o laravel com docker

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -26,22 +26,102 @@
 
 {{-- Unless é o contrario do if/else --}}
 {{-- Unless executa se o retorno for false --}}
-Fornecedor = {{$fornecedores[0]["Nome"]}}
-<br />
-Status = {{$fornecedores[0]["Status"]}}
-<br />
-@isset($fornecedores[0]["CNPJ"])
-CNPJ = {{$fornecedores[0]["CNPJ"]}}
+{{--@for($a=0;isset($fornecedores[$a]);$a++)
+    Fornecedor = {{$fornecedores[$a]["Nome"]}}
+    <br />
+    Status = {{$fornecedores[$a]["Status"]}}
+    <br />
+    CNPJ = {{$fornecedores[$a]["CNPJ"] ?? "CNPJ não preenchido"}}
+    <br />
+    Telefone = ({{$fornecedores[$a]["DDD"] ?? ""}}) {{$fornecedores[$a]["Telefone"] ?? ""}}
+    <br />
+    Cidade = 
+    @switch($fornecedores[$a]["DDD"])
+        @case("51")
+            São Paulo/SP
+            @break
+        @case("84")
+            Rio de Janeiro/RJ
+            @break
+        @case("87")
+            Porto Alegre/RS
+            @break
+        @default
+            Estado não informado
+    @endswitch
+    <br />
+    <hr />
+@endfor--}}
+{{--@php $a = 0 @endphp
+@while(isset($fornecedores[$a]))
+    Fornecedor = {{$fornecedores[$a]["Nome"]}}
+    <br />
+    Status = {{$fornecedores[$a]["Status"]}}
+    <br />
+    CNPJ = {{$fornecedores[$a]["CNPJ"] ?? "CNPJ não preenchido"}}
+    <br />
+    Telefone = ({{$fornecedores[$a]["DDD"] ?? ""}}) {{$fornecedores[$a]["Telefone"] ?? ""}}
+    <br />
+    Cidade = 
+    @switch($fornecedores[$a]["DDD"])
+        @case("51")
+            São Paulo/SP
+            @break
+        @case("84")
+            Rio de Janeiro/RJ
+            @break
+        @case("87")
+            Porto Alegre/RS
+            @break
+        @default
+            Estado não informado
+    @endswitch
+    <br />
+    <hr />
+    @php $a++ @endphp
+@endwhile--}}
+@foreach($fornecedores as $indice => $fornecedor)
+    Fornecedor = {{$fornecedor["Nome"]}}
+    <br />
+    Status = {{$fornecedor["Status"]}}
+    <br />
+    CNPJ = {{$fornecedor["CNPJ"] ?? "CNPJ não preenchido"}}
+    <br />
+    Telefone = ({{$fornecedor["DDD"] ?? ""}}) {{$fornecedor["Telefone"] ?? ""}}
+    <br />
+    Cidade = 
+    @switch($fornecedor["DDD"])
+        @case("51")
+            São Paulo/SP
+            @break
+        @case("84")
+            Rio de Janeiro/RJ
+            @break
+        @case("87")
+            Porto Alegre/RS
+            @break
+        @default
+            Estado não informado
+    @endswitch
+    <br />
+    <hr />
+@endforeach
+
+{{--@isset($fornecedores[0]["CNPJ"])
+    CNPJ = {{$fornecedores[0]["CNPJ"]}}
+    @empty($fornecedores[0]["CNPJ"])
+        VAZIO
+    @endempty
 <br />
 @endisset
 {{-- Sintaxe IF --}}
 @if($fornecedores[0]["Status"]=="S")
-    Fornecedor Ativo
+    {{--Fornecedor Ativo --}}
 @endif
 
 {{-- Sintaxe UNLESS --}}
 @unless($fornecedores[0]["Status"]=="S")
-    Fornecedor Inativo
+    {{--Fornecedor Inativo--}}
 @endunless
 
 {{-- Exibir arrays no front --}}
